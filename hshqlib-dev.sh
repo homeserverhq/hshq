f56a8aa41a56245b0271d306823042da8c0c96ca
HomeServerHQ <118991087+homeserverhq@users.noreply.github.com>
2026-08-17 05:51:01 -0500

    Version 238
     - Fix last commit message

--- hshqlib-dev.sh
+++ hshqlib-dev.sh
@@ -2,7 +2,6 @@
 HSHQ_LIB_SCRIPT_VERSION=238
 LOG_LEVEL=info
 
-
 # Copyright (C) 2023 HomeServerHQ <drdoug@homeserverhq.com>
 #
 # This program is free software: you can redistribute it and/or modify
